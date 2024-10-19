5c123cbf14c6d6dd73c88d2fde1c9ebad5600055
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-10-19 14:17:52 +0530

    Improvement 06

---
+++ Practice/For of Loop.js
@@ -0,0 +1,22 @@
+console.log("For of Loop :- \n");
+
+// Appling on Array
+let colors = ["Pink", "Red", "Orange", "Black", "Blue"];
+
+console.log("1st Attempt on Array :");
+for(color of colors) {
+    console.log(color);
+}
+
+console.log("\n2nd Attempt on Array :");
+for(colo of colors) {
+    console.log(colo);
+}
+
+// Appling on String 
+let name = "Chandan";
+
+console.log("\nOn String :");
+for(char of name) {
+    console.log(char);
+}
