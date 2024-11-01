29038adf489b56baa75ad60fc1dd326ee8efd79c
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-01 09:36:32 +0530

    Improvement 15

---
+++ Practice/Mouse Event 01.js
@@ -0,0 +1,17 @@
+let btn1 = document.querySelector(".btn1");
+
+btn1.addEventListener("click", () => {
+    console.log("Hello Chandan");
+})
+
+let btn2 = document.querySelector(".btn2");
+
+btn2.addEventListener("dblclick", () => {
+    console.log("Hello Double Chandan");
+})
+
+let btn3 = document.querySelector(".btn3");
+
+btn3.addEventListener("mouseenter", () => {
+    console.log("Hello Chandan Enter");
+})
