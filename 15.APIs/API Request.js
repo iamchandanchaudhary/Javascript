7ba31ef23afb813f82614e4690b1099fd8bd92d2
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-15 08:35:48 +0530

    Upload 27

---
+++ 15.APIs/API Request.js
@@ -0,0 +1,35 @@
+let url = "https://catfact.ninja/fact";
+
+console.log(fetch(url)); // ==> Visibal in Network Tab
+
+/* Not Visible in Console Window */
+// fetch(url)
+// .then((response) => {
+//     console.log(response);
+// })
+// .catch( (err) => {
+//     console.log(err);
+// })
+
+/* Visibal in Promise Tab */
+// fetch(url)
+// .then((response) => {
+//     // console.log(response);
+//     console.log(response.json());
+// })
+// .catch( (err) => {
+//     console.log("Error -", err);
+// })
+
+fetch(url)
+.then((response) => {
+    // console.log(response.json());
+    return response.json();
+})
+.then( (data) => {
+    console.log(data);
+    console.log(data.fact);
+})
+.catch( (err) => {
+    console.log("Error -", err);
+})
