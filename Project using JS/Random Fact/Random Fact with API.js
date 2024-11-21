b4cc241b0283dce34bf441ba81f237ca70942c5f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-21 08:44:05 +0530

    Upload 31

--- Project using JS/Random Fact/Random Fact with API.js
+++ Project using JS/Random Fact/Random Fact with API.js
@@ -4,50 +4,7 @@ let factP = document.querySelector(".fact-p");
 let gBtn = document.querySelector(".generate-btn");
 let ul = document.querySelector("ul");
 
-// fetch(url)
-// .then((response) => {
-//     // console.log(response.json());
-//     return response.json();
-// })
-// .then( (data) => {
-//     console.log(data);
-//     console.log(data.fact);
-
-//     // Adding Fact in Paragraph
-//     // factP.innerText = data.fact;
-
-//     gBtn.addEventListener("click", () => {
-//         // factP.innerText = data.fact;
-//         let newli = document.createElement("li");
-//         newli.innerText = data.fact;
-
-//         ul.appendChild(newli);
-//         return fetch(url);
-//     });
-// })
-// .catch( (err) => {
-//     console.log("Error -", err);
-// })
-
-// console.log("Chandan Chaudhary");
-
-// let url = "https://catfact.ninja/fact";
-
-// async function getFact() {
-//     try {
-//         let result = await fetch(url);
-//         let data = await result.json();
-
-//         console.log(data);
-//         console.log(data.fact);
-
-//         // return data.fact;
-//     }
-//     catch(err) {
-//         console.log("Error -", err)
-//     }
-
-// }
+let clearBtn = document.querySelector(".clear-btn");
 
 gBtn.addEventListener("click", async () => {
     
@@ -68,13 +25,14 @@ async function getFact() {
     
         ul.appendChild(newli);
 
+        // Reset Btn
+        clearBtn.addEventListener("click", async () => {
+            newli.remove();
+        });
+
     }
     catch(err) {
         console.log("Error -", err);
         console.log("Fact not Found");
     }
 }
-// let data = getFact();
-// console.log(data);
-
-// getFact();
