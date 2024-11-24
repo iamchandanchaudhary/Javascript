43e1e852b75eb1fa49cebb48b586231212b65bb9
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-24 09:11:21 +0530

    Improvement 24

---
+++ Practice/Practice 07.js
@@ -0,0 +1,21 @@
+console.log("Chandan");
+
+let student = {
+    Chandan: {
+        age: 18,
+        marks: 98.7
+    },
+
+    Aman: {
+        age: 21,
+        marks: 91
+    },
+
+    Vishal: {
+        age: 23,
+        marks: 78
+    }
+}
+
+console.log(student);
+console.log("Chandan Details :", student.Chandan);
