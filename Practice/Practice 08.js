5863334a9bb645e66f713619619299812463203f
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-29 08:55:28 +0530

    Improvement 25

---
+++ Practice/Practice 08.js
@@ -0,0 +1,14 @@
+console.log("For of Loop:- \n");
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+for(n of number) {
+    console.log(n);
+}
+
+let arr = ["Chandan", "Bipin", "Kajal", "Suraj"];
+
+console.log(arr);
+for(a of arr) {
+    console.log(a);
+}
