5fc6f3ae1d3eefb0888b4c10c2a2ce0f83b028c1
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-03 18:12:37 +0530

    Improvement 29

---
+++ Practice/API Fetch.js
@@ -0,0 +1,13 @@
+let url = "https://catfact.ninja/fact";
+
+// JS TO JSON
+let student = {
+    name: "Chandan",
+    age: 18
+};
+
+console.log(student);
+
+let jsonValue = JSON.stringify(student);
+
+console.log(jsonValue);
