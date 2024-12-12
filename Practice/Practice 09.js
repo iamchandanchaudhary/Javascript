3c7fa4d3477205c6d08165b1d62dcc80b6a302c7
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-12 20:20:52 +0530

    Improvement 40

---
+++ Practice/Practice 09.js
@@ -0,0 +1,19 @@
+let student = {
+    chandan: {
+        age: 18,
+        rollno: 230010
+    },
+
+    Aman: {
+        age: 19,
+        rollno: 230011
+    },
+
+    Vivek: {
+        age: 21,
+        rollno: 230012
+    }
+}
+
+console.log(student);
+console.log(student.chandan);
