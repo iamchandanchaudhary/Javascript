3bcde749f97f73e3270fb211d61b2732743934e6
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-27 17:54:07 +0530

    Improvement 50

---
+++ Project using JS/About-me/src/app.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".btn");
+let container = document.querySelector(".container");
+
+btn.addEventListener("click", () => {
+    container.classList.add("open-popup");
+})
