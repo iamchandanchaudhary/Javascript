27bfe222f8c04429a67cadf4bac59fd5460acbd0
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-29 18:16:52 +0530

    Portfolio Improvement 25

--- Project using JS/Portfolio/src/app.js
+++ Project using JS/Portfolio/src/app.js
@@ -2,10 +2,26 @@ let btn = document.querySelector(".about-btn");
 let container = document.querySelector(".container");
 let closeBtn = document.querySelector(".close-btn");
 
+//  Pop-up Function
 btn.addEventListener("click", () => {
     container.classList.add("open-popup");
 })
 
 closeBtn.addEventListener("click", () => {
     container.classList.remove("open-popup");
+})
+
+// Back to Top & Scroll
+let dragBtn = document.querySelector(".drag-btn");
+
+let move = document.querySelector(".move");
+
+dragBtn.addEventListener("click", () => {
+    window.scrollTo(0, 700);
+})
+
+let backtoTop = document.querySelector(".backtop");
+
+backtoTop.addEventListener("click", () => {
+    window.scrollTo(0, 0);
 })
