272ed1652d62ead320898241a659616f12975b2e
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-30 09:56:02 +0530

    Portfolio Improvement 26

--- Project using JS/Portfolio/src/script.js
+++ Project using JS/Portfolio/src/script.js
@@ -25,3 +25,24 @@ let backtoTop = document.querySelector(".backtop");
 backtoTop.addEventListener("click", () => {
     window.scrollTo(0, 0);
 })
+
+// Dark & Light Button
+let darkBtn = document.querySelector(".dark-btn");
+let lightBtn = document.querySelector(".light-btn");
+let body = document.querySelector("body");
+
+lightBtn.addEventListener("click", () => {
+    darkBtn.classList.add("visible");
+    darkBtn.classList.remove("hidden");
+    lightBtn.classList.add("hidden");
+
+    // body.style.background = "#fff";
+})
+
+darkBtn.addEventListener("click", () => {
+    lightBtn.classList.add("visible");
+    lightBtn.classList.remove("hidden");
+    darkBtn.classList.add("hidden");
+
+    // body.style.background = "#111";
+})
