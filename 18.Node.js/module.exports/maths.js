a95fc8e7807ea02db0ed003af5f6a421b9bcf92d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-01-01 11:07:51 +0530

    Upload 51

---
+++ 18.Node.js/module.exports/maths.js
@@ -0,0 +1,12 @@
+let PI = 3.14;
+let g = 9.8;
+
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = 124;
---
+++ 18.Node.js/module.exports/script.js
@@ -0,0 +1,3 @@
+let someValue = require("./maths");
+
+console.log(someValue);
